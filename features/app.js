b47e69e7a19425151fb8517b68491c5d504cc3be
laobanwang676-droid <laobanwang676@gmail.com>
2026-09-08 09:36:26 +0800

    修复bug

--- features/app.js
+++ features/app.js
@@ -434,6 +434,7 @@ const API_BASE = 'https://7b048004d78a4e86aa4c7f1eb2dfab31.hn.takin.cc';
     }
 
     function setVerifyStatus(text) {
+      verifyStatus.className = '';
       verifyStatus.textContent = text;
     }
 
